eff1368c2caec8434734c1c20869b8067a595c0c
Swastik <161652674+swastik7015@users.noreply.github.com>
2025-01-02 22:57:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
 # Mario character properties
-mario_width, mario_height = 50, 50
-mario_x, mario_y = 100, HEIGHT - mario_height - 50
+mario_width, mario_height = 70, 70
+mario_x, mario_y = 100, HEIGHT - mario_height - 107
 mario_speed = 5
 jump_height = 15
 is_jumping = False
 velocity_y = 0
 
 # Load Mario image
-mario_image = pygame.image.load('mario.png')
+mario_image = pygame.image.load('mario_image.png')
 mario_image = pygame.transform.scale(mario_image, (mario_width, mario_height))
 
 # Load obstacle image
 obstacle_image_path = 'cactus.jpeg'
 obstacle_image = pygame.image.load(obstacle_image_path)
-obstacle_image = pygame.transform.scale(obstacle_image, (40, 40))
+obstacle_image = pygame.transform.scale(obstacle_image, (60,60))
 
 # Load background image
-background_image_path = 'background.jpeg'  # Replace with the actual path
+background_image_path = 'background2.gif'  # Replace with the actual path
 background_image = pygame.image.load(background_image_path)
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
@@ -46,7 +46,7 @@ background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 gravity = 0.8
 
 # Obstacle properties
-obstacle_width, obstacle_height = 40, 40
+obstacle_width, obstacle_height = 30, 115
 obstacle_x = WIDTH
 obstacle_y = HEIGHT - 50 - obstacle_height
 obstacle_speed = 5
@@ -70,7 +70,7 @@ def show_game_over():
 def reset_game():
     global mario_x, mario_y, is_jumping, velocity_y, obstacle_x
     mario_x = 100
-    mario_y = HEIGHT - mario_height - 50
+    mario_y = HEIGHT - mario_height - 107
     is_jumping = False
     velocity_y = 0
     obstacle_x = WIDTH
@@ -116,8 +116,8 @@ def main():
             if is_jumping:
                 mario_y += velocity_y
                 velocity_y += gravity
-                if mario_y >= HEIGHT - mario_height - 50:
-                    mario_y = HEIGHT - mario_height - 50
+                if mario_y >= HEIGHT - mario_height - 100:
+                    mario_y = HEIGHT - mario_height - 107
                     is_jumping = False
 
             # Prevent Mario from going out of bounds
@@ -141,4 +141,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
